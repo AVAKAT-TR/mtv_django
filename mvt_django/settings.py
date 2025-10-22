f37b7da486934eeb87fd61f32f39092340504db2
AVAKAT-TR <pablotr54k1@gmail.com>
2025-10-22 12:18:52 -0300

    Cambiar a SQLite para Render

--- mvt_django/settings.py
+++ mvt_django/settings.py
@@ -74,21 +74,37 @@ WSGI_APPLICATION = 'mvt_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
+#DATABASES = {
     #'default': {
     #    'ENGINE': 'django.db.backends.sqlite3',
    #     'NAME': BASE_DIR / 'db.sqlite3',
   #  }
 
-'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ORDEXADB',
-        'USER': 'root',
-        'PASSWORD': 'Inacap.2024',
-        'HOST': 'localhost',
-        'PORT': '3306',
+
+
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
+
+
+
+#'default': {
+       # 'ENGINE': 'django.db.backends.mysql',
+      #  'NAME': 'ORDEXADB',
+     #   'USER': 'root',
+    #    'PASSWORD': 'Inacap.2024',
+   #     'HOST': 'localhost',
+  #      'PORT': '3306',
+ #   }
+#}
 
 
 # Password validation
